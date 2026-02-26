964df97d5857387a67ea1ae94b667d235620d9f2
brohd <brohdie_larocque@hotmail.com>
2026-02-26 04:01:20 -0800

    update utils paths

--- src/utils/console_utils_remote.gd
+++ src/utils/console_utils_remote.gd
@@ -1,10 +1,10 @@
 #! remote
 
 const DockManager = preload("res://addons/addon_lib/brohd/dock_manager/dock_manager.gd")
-
 const BottomPanel = preload("res://addons/addon_lib/brohd/alib_editor/utils/src/editor_nodes/bottom_panel.gd")
-
 const PopupHelper = preload("res://addons/addon_lib/brohd/alib_runtime/popup_menu/popup_menu_path_helper.gd")
-const UFile = preload("res://addons/addon_lib/brohd/alib_runtime/utils/src/u_file.gd")
-const UNode = preload("res://addons/addon_lib/brohd/alib_runtime/utils/src/u_node.gd")
-const UClassDetail = preload("res://addons/addon_lib/brohd/alib_editor/utils/src/u_class_detail.gd")
+
+const UClassDetail = preload("uid://gyx3o6jv751x") # u_class_detail.gd
+
+const UFile = preload("uid://gs632l1nhxaf") # u_file.gd
+const UNode = preload("uid://dsywt12xnn7oh") # u_node.gd
