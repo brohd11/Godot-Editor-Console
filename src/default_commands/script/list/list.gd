643a6b77c21dd05751b47c6659ea96956fc2b520
brohd <brohdielr@proton.me>
2026-07-15 14:52:40 -0700

    rework list command

--- src/default_commands/script/list/list.gd
+++ src/default_commands/script/list/list.gd
@@ -6,19 +6,53 @@ const EditorColors = UtilsRemote.EditorColors
 const ScriptUtil = preload("res://addons/editor_console/src/default_commands/script/script_util.gd")
 
 const LIST_COMMANDS_OPTIONS = ["--methods", "--signals", "--constants", "--properties", "--enums"]
-const LIST_MODIFIER_OPTIONS = ["--lines", "--data", "--inherited"]
+const LIST_MODIFIER_OPTIONS = ["--data", "--inherited", "--pretty"]
 
 const LIST_OPTION_HELP = {
-	"--methods": "List the script's methods.",
-	"--signals": "List the script's signals.",
-	"--constants": "List the script's constants.",
-	"--properties": "List the script's properties.",
-	"--enums": "List the script's enums (requires --inherited).",
-	"--lines": "Print one member per line.",
-	"--data": "Print each member's details.",
-	"--inherited": "Include inherited members from the base class.",
+	"--methods":{
+		"help":"List the script's methods.",
+		"prop": &"method_flag",
+	},
+	"--signals": {
+		"help": "List the script's signals.",
+		"prop": &"signal_flag",
+		},
+	"--constants": {
+		"help":"List the script's constants.",
+		"prop": &"const_flag"
+		},
+	"--properties": {
+		"help":"List the script's properties.",
+		"prop": &"prop_flag"
+		},
+	"--enums": {
+		"help":"List the script's enums (requires --inherited).",
+		"prop": &"enum_flag"
+		},
+	"--data": {
+		"help":"Print each member's details.",
+		"prop": &"data_flag"
+		},
+	"--inherited": {
+		"help":"Include inherited members from the base class.",
+		"prop":&"inh_flag"
+		},
+	"--pretty": {
+		"help":"Print on a single line rather than new-lines.",
+		"prop": &"pretty_flag"
+		},
 }
 
+var target_all_flag:=true
+var method_flag:=false
+var signal_flag:=false
+var const_flag:=false
+var prop_flag:=false
+var enum_flag:=false
+
+var inh_flag:=false
+var data_flag:=false
+var pretty_flag:=false
 
 static func get_command_name() -> String:
 	return "list"
@@ -27,142 +61,127 @@ static func get_command_name() -> String:
 static func get_self_command_data() -> Dictionary:
 	return Options.get_single_option_dict(get_command_name(), {
 		&"help": "List properties of target script\nUsage: script list <options>",
-		&"get_command": func(): return new()
 	})
 
 func _get_flags() -> Dictionary:
 	var options = Options.new()
 	for cmd in LIST_COMMANDS_OPTIONS:
 		options.add_option(cmd, {
-			&"help": LIST_OPTION_HELP.get(cmd, "")
+			&"help": LIST_OPTION_HELP.get(cmd, {}).get("help", "")
 		})
 
 	#if options.size() < LIST_COMMANDS_OPTIONS.size():
 	options.add_separator("Modifiers")
 	for cmd in LIST_MODIFIER_OPTIONS:
 		options.add_option(cmd, {
-			&"help": LIST_OPTION_HELP.get(cmd, "")
+			&"help": LIST_OPTION_HELP.get(cmd, {}).get("help", "")
 		})
 	
 	return options.get_options()
 
+func _process_flag(flag:String):
+	var prop = LIST_OPTION_HELP.get(flag, {}).get("prop")
+	set(prop, true)
+	if flag in LIST_COMMANDS_OPTIONS:
+		target_all_flag = false
 
-
-
-func _get_completions(ctx:CompletionContext):
-	var flags = get_flags(true)
-	var has_valid_flag = false
-	for c in consumed_tokens:
-		if c in LIST_COMMANDS_OPTIONS:
-			has_valid_flag = true
-			break
-	if not has_valid_flag:
-		for c in LIST_MODIFIER_OPTIONS:
-			flags.erase(c)
-	return flags
 
 func _execute(ctx:CompletionContext):
 	consumed_tokens.erase("list")
-	if consumed_tokens.is_empty():
-		_get_help_for_token("list")
-		return
-	if not ctx.unconsumed_tokens.is_empty():
-		ctx.append_error("Unrecognized commands: " + str(ctx.unconsumed_tokens))
-		return 1
 	var script = ScriptUtil.get_script_from_ctx(ctx)
 	var script_name = UClassDetail.get_global_class_name(script.resource_path)
 	if script_name == "":
 		script_name = script.resource_path.get_file()
-	return print_members(ctx, script_name, consumed_tokens, script)
+	return list_members(ctx, script_name, script)
 
 
-static func print_members(ctx:CompletionContext, script_name:String, flags:Array, script:Script) -> int:
-	var print_data = LIST_MODIFIER_OPTIONS[1] in flags
-	var print_lines = LIST_MODIFIER_OPTIONS[0] in flags
-	var inherited = LIST_MODIFIER_OPTIONS[2] in flags
-	for cmd in LIST_MODIFIER_OPTIONS:
-		flags.erase(cmd)
-	var valid = false
-	for a in flags:
-		if a in LIST_COMMANDS_OPTIONS:
-			valid = true
-	var flags_size = flags.size()
-	if not valid:
-		if flags_size > 0:
-			ctx.append_error("'--data', '--lines', and '--inherited' should be passed with another argument.")
-		else:
-			ctx.append_error("Pass arguments for the list command.")
-		return ExitCode.ERR
+func list_members(ctx:CompletionContext, script_name:String, script:Script) -> int:
+	var print_pretty = pretty_flag
+	var print_data = data_flag
+	var inherited = inh_flag
+	
 	
 	var pr = Pr.new()
-	for i in range(flags_size):
-		var command = flags[i]
-		if command in LIST_COMMANDS_OPTIONS:
-			var members = {}
-			if command == LIST_COMMANDS_OPTIONS[0]: # methods
-				if inherited:
-					ctx.append_output("Printing class methods: %s" % script_name)
-					members = UClassDetail.class_get_all_methods(script)
-				else:
-					ctx.append_output("Printing script methods: %s" % script_name)
-					members = UClassDetail.script_get_all_methods(script)
-			elif command == LIST_COMMANDS_OPTIONS[1]: # signals
-				if inherited:
-					ctx.append_output("Printing class signals: %s" % script_name)
-					members = UClassDetail.class_get_all_signals(script)
-				else:
-					ctx.append_output("Printing script signals: %s" % script_name)
-					members = UClassDetail.script_get_all_signals(script)
-			elif command == LIST_COMMANDS_OPTIONS[2]: # constants
-				if inherited:
-					ctx.append_output("Printing class constants: %s" % script_name)
-					members = UClassDetail.class_get_all_constants(script)
-				else:
-					ctx.append_output("Printing script constants: %s" % script_name)
-					members = UClassDetail.script_get_all_constants(script)
-			elif command == LIST_COMMANDS_OPTIONS[3]: # properties
-				if inherited:
-					ctx.append_output("Printing class properties: %s" % script_name)
-					members = UClassDetail.class_get_all_properties(script)
-				else:
-					ctx.append_output("Printing script properties: %s" % script_name)
-					members = UClassDetail.script_get_all_properties(script)
-			elif command == LIST_COMMANDS_OPTIONS[4]: # enums
-				if inherited:
-					ctx.append_output("Printing class enums: %s" % script_name)
-					members = UClassDetail.class_get_all_enums(script)
-				else:
-					ctx.append_error("Cannot get script enums, no API in ClassDB. Use '--inherited' option.")
-					#members = UClassDetail.sc(script)
-					pass
-			
-			if members.is_empty():
-				pr.append("\tNone in script.", Colors.VAR_RED)
-				ctx.append_output(pr.get_string(true))
-			else:
-				if print_lines or print_data:
-					for m in members.keys():
-						pr.append("%s" % m, Colors.ACCENT_MUTE)
-						ctx.append_output(pr.get_string(true))
-						if print_data:
-							var data = members.get(m)
-							if data == null:
-								pr.append("\tNo data.")
-								ctx.append_output(pr.get_string(true))
-							elif data is String:
-								ctx.append_output("\t" + data)
-							elif data is Dictionary:
-								for key in data.keys():
-									pr.append("\t%s - %s" % [key, data[key]], Colors.GRAY)
-									ctx.append_output(pr.get_string(true))
-							else:
-								ctx.append_output("\t" + str(data))
-				else:
-					pr.append("\t" + "  ".join(members.keys()), Colors.ACCENT_MUTE)
-					ctx.append_output(pr.get_string(true))
-				
-			if i < flags_size - 1:
-				ctx.append_output("") # print blank line between sections
+	for flag in LIST_OPTION_HELP.keys():
+		if not flag in LIST_COMMANDS_OPTIONS:
 			continue
+		var prop = LIST_OPTION_HELP.get(flag).get("prop")
+		if not (get(prop) == true or target_all_flag):
+			continue
+		
+		var flag_raw = flag.trim_prefix("--")
+		var members = _get_members(script, flag, inherited)
+		if inherited:
+			ctx.append_output("Class %s:" % [flag_raw])
+		else:
+			if flag == "--enums":
+				if not target_all_flag:
+					ctx.append_output("\tCannot get 'Script' enums, no API in ClassDB. Use '--inherited' option.")
+				continue
+			ctx.append_output("Script %s:" % [flag_raw])
+			
+		
+		_add_to_members_to_output(ctx, members, pr, print_data, print_pretty)
 	
+	ctx.strip_output_newlines()
 	return ExitCode.OK
+
+static func _get_members(script:GDScript, flag:String, inherited:bool):
+	match flag:
+		LIST_COMMANDS_OPTIONS[0]: # methods
+			if inherited:
+				return UClassDetail.class_get_all_methods(script)
+			else:
+				return UClassDetail.script_get_all_methods(script)
+		LIST_COMMANDS_OPTIONS[1]: # signals
+			if inherited:
+				return UClassDetail.class_get_all_signals(script)
+			else:
+				return UClassDetail.script_get_all_signals(script)
+		LIST_COMMANDS_OPTIONS[2]: # constants
+			if inherited:
+				return UClassDetail.class_get_all_constants(script)
+			else:
+				return UClassDetail.script_get_all_constants(script)
+		LIST_COMMANDS_OPTIONS[3]: # properties
+			if inherited:
+				return UClassDetail.class_get_all_properties(script)
+			else:
+				return UClassDetail.script_get_all_properties(script)
+		LIST_COMMANDS_OPTIONS[4]: # enums
+			if inherited:
+				return UClassDetail.class_get_all_enums(script)
+			else:
+				return {}
+			
+
+static func _add_to_members_to_output(ctx, members, pr, print_data:bool, pretty:bool):
+	if members.is_empty():
+		pr.append("\tNone in script.", Colors.VAR_RED)
+		ctx.append_output(pr.get_string(true))
+	else:
+		if pretty and not print_data:
+			pr.append("\t" + "  ".join(members.keys()), Colors.ACCENT_MUTE)
+			ctx.append_output(pr.get_string(true))
+		
+		else:
+			for m in members.keys():
+				var member_string = "%s" % m
+				var member_color = Colors.ACCENT_MUTE if pretty else Color.TRANSPARENT
+				pr.append("\t%s" % m, member_color)
+				ctx.append_output(pr.get_string(true))
+				if print_data:
+					var data = members.get(m)
+					if data == null:
+						pr.append("\t\tNo data.")
+						ctx.append_output(pr.get_string(true))
+					elif data is String:
+						ctx.append_output("\t\t" + data)
+					elif data is Dictionary:
+						for key in data.keys():
+							pr.append("\t\t%s - %s" % [key, data[key]], Colors.GRAY)
+							ctx.append_output(pr.get_string(true))
+					else:
+						ctx.append_output("\t\t" + str(data))
+	
+	ctx.append_output("")
